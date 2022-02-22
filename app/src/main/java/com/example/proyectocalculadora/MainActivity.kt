5c48f87f33cf43d20cfa544b882d5a5a792e062a
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-02-22 10:17:16 -0600

    Functional operations with integer numbers
    Added functionality to the addition, subtraction, multiplication and division buttons. It only works with integers numbers and with operations of two arguments.

--- app/src/main/java/com/example/proyectocalculadora/MainActivity.kt
+++ app/src/main/java/com/example/proyectocalculadora/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
+import kotlin.math.log
 
 private val TAG = "MainActivity"
 
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private var userInput: TextView? = null
     private var calcHist: TextView? = null
+    val input = mutableListOf<Int>()
+    var inputFloat: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,13 +37,28 @@ class MainActivity : AppCompatActivity() {
         val button9: Button = findViewById<Button>(R.id.button_9)
         val button0: Button = findViewById<Button>(R.id.button_0)
 
+        //Operations Buttons
+        val buttonC: Button = findViewById<Button>(R.id.button_C)
+        val buttonSum: Button = findViewById<Button>(R.id.button_plus)
+        val buttonMin: Button = findViewById<Button>(R.id.button_minus)
+        val buttonMult: Button = findViewById<Button>(R.id.button_multiplication)
+        val buttonDiv: Button = findViewById<Button>(R.id.button_division)
+        val buttonEqu: Button = findViewById<Button>(R.id.button_equal)
 
+        val buttonDot: Button = findViewById<Button>(R.id.button_dot)
+
+        var oper: Int = 0
+        var num1: Int = 0
+        var num2: Int = 0
+        var result: Int = 0
+        var calcHistTxt: String = ""
 
         //Button 1
         button1?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 1")
                 userInput?.append("1")
+                input.add(1)
             }
         })
 
@@ -49,6 +67,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 2")
                 userInput?.append("2")
+                input.add(2)
             }
         })
 
@@ -57,6 +76,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 3")
                 userInput?.append("3")
+                input.add(3)
             }
         })
 
@@ -65,6 +85,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 4")
                 userInput?.append("4")
+                input.add(4)
             }
         })
 
@@ -73,6 +94,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 5")
                 userInput?.append("5")
+                input.add(5)
             }
         })
 
@@ -81,6 +103,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 6")
                 userInput?.append("6")
+                input.add(6)
             }
         })
 
@@ -89,6 +112,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 7")
                 userInput?.append("7")
+                input.add(7)
             }
         })
 
@@ -97,6 +121,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 8")
                 userInput?.append("8")
+                input.add(8)
             }
         })
 
@@ -105,6 +130,7 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 9")
                 userInput?.append("9")
+                input.add(9)
             }
         })
 
@@ -113,6 +139,94 @@ class MainActivity : AppCompatActivity() {
             override fun onClick(p0: View?) {
                 Log.d(TAG, "onClick 0")
                 userInput?.append("0")
+                input.add(0)
+            }
+        })
+
+        //Button dot
+        buttonDot?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick dot")
+                userInput?.append(".")
+            }
+        })
+
+        //Button C
+        buttonC?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick C")
+                userInput?.setText("")
+                calcHist?.setText("")
+            }
+        })
+
+        //Button plus
+        buttonSum?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick Sum")
+                num1 = input.removeAt(0)
+                oper = 1
+                userInput?.append(" + ")
+                calcHist?.setText(num1.toString())
+            }
+        })
+
+        //Substract
+        buttonMin?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick Min")
+                num1 = input.removeAt(0)
+                oper = 2
+                userInput?.append(" - ")
+            }
+        })
+
+        //Multiplicate
+        buttonMult?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick Mult")
+                num1 = input.removeAt(0)
+                oper = 3
+                userInput?.append(" x ")
+            }
+        })
+
+        //Division
+        buttonDiv?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick Div")
+                num1 = input.removeAt(0)
+                oper = 4
+                userInput?.append(" ÷ ")
+            }
+        })
+
+        //Calculation
+        buttonEqu.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d(TAG, "onClick Equal")
+                num2 = input.removeAt(0)
+
+                if(oper == 1) {
+                    result = num1 + num2
+                    //calcHist?.setText(num1, )
+                    calcHistTxt = num1.toString() + " + " + num2.toString()
+                }
+                else if (oper == 2) {
+                    result = num1 - num2
+                    calcHistTxt = num1.toString() + " - " + num2.toString()
+                }
+                else if (oper == 3) {
+                    result = num1 * num2
+                    calcHistTxt = num1.toString() + " x " + num2.toString()
+                }
+                else if (oper == 4) {
+                    result = num1 / num2
+                    calcHistTxt = num1.toString() + " ÷ " + num2.toString()
+                }
+                userInput?.setText(result.toString())
+                calcHist?.setText(calcHistTxt)
+                input.add(result)
             }
         })
     }
